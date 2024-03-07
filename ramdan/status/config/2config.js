5b613f50384e0f68bfec80d85c2830ac956fd35d
MikroTechnique <MikroTechnique@users.noreply.github.com>
2024-03-07 14:27:40 +0300

    none

---
+++ ramdan/status/config/2config.js
@@ -0,0 +1,70 @@
+Config({
+    "network-name": "ZAIN 4G",
+    "service-number": "773880310",
+    "login-type": "user", 
+    "speed-select": 1 ,
+    "speed-var":"domain",
+    "speed-option":`
+    <option value="" selected disabled hidden>أختيار سرعة الإنترنت</option>
+    <option value="" >سرعة أفتراضية </option>
+    <option value="128K/512K">سرعة منخفضة 512 </option>
+    <option value="256K/1024K">سرعة عادية 1 ميجا</option>
+    <option value="512K/2048K">سرعة متوسطة 2 ميجا</option>
+    <option value="1024K/4096K">سرعة عالية جدا 4 ميجا</option>`,
+    "news-line": "شبكتنا ترحب بكم و تبشركم بتوفر فئات كروت السرعات العالية VIP *  تذكر أن الله ينظر إليك فلا تجعله أهون الناظرين إليك  " ,
+    "input-autocomplete": "on",
+    "input-type": "text",
+    "input-rm-white-spaces": 1,
+    "input-to-lower": 0,
+    "input-to-upper": 0,
+    "input-to-arabic-numbers": 1,
+    "input-only-numbers": 0,
+    "input-no-numbers": 0,
+    "input-only-alphanumeric": 0,
+    "enable-hot-cookie": 1 ,
+    "enable-hot-blocker": 1,
+    "clear-router-cookie": 1,
+    "clear-hot-cookie": 1,
+    "block-time": 2,
+    "try-count": 5,
+    "warn-when": 3,
+    "warn-message": "تحذير !! عدد محاولاتك الخاطئة اصبح {{tryCounter}} محاولات, عدد المحاولات المسموح بها هي {{tryCount}} محاولات فقط, عدد محاولاتك المتبقية {{restTryCount}} محاولات, سيتم حظرك لمدة {{blockTime}} دقائق اذا تجاوزت العدد المسموح للمحاولات",
+    "price-button": true,
+    "sell-point-button": true,
+    "show-date-field": true,
+    "loan-button": true,
+    "redirect-to-esterahah": "",// orWithProtocol: ex, https://google.com leave it blank if dont need esterahah button
+    "redirect-to-mobasher": "",// orWithProtocol: ex, https://google.com leave it blank if dont need mobasher button
+    "profiles": [
+        {
+            "price": "100 ريال",
+            "time": "3 ساعات",
+            "transfer": "250 ميجا",
+            "validity": "3 ايام",
+        },
+        {
+            "price": "200 ريال",
+            "time": "7 ساعات",
+            "transfer": "600 ميجا",
+            "validity": "7 ايام",
+        },
+        {
+            "price": "250 ريال",
+            "time": "8 ساعات",
+            "transfer": "700 ميجا",
+            "validity": "7 ايام",
+        },
+        {
+            "price": "500 ريال",
+            "time": "18 ساعة",
+            "transfer": "1500 ميجا",
+            "validity": "10 ايام",
+        },
+    ],
+    "sell-points": [
+        {"name":"تموينات الباشا"},
+        {"name":"تموينات أمين السهمي"},
+        {"name":"تموينات مازن السهمي"},
+        {"name":"تموينات فؤاد"},
+    ]
+})
